20ede3cd553e46b13560f91bc9b9c218195f7608
Steven <29103029+Stedoss@users.noreply.github.com>
2019-10-15 14:53:11 +0100

    Add A* Implementation

--- src/matcher_starter/core.clj
+++ src/matcher_starter/core.clj
@@ -3,7 +3,73 @@
             [org.clojars.cognesence.matcher.core :refer :all]
             [org.clojars.cognesence.ops-search.core :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def graph
+  "Map of node => set of adjacent nodes."
+  {"alpha" #{"charlie", "beta"}
+   "beta" #{"alpha", "delta"}
+   "charlie" #{"alpha", "echo", "delta"}
+   "delta" #{"beta", "charlie", "foxtrot"}
+   "echo" #{"charlie", "foxtrot"}
+   "foxtrot" #{"echo", "delta"}})
+
+(def costs
+  "Map of node => adjacent node => cost. This could
+   be replaced with any cost function of the shape
+   (node, node') => cost."
+  {"alpha" {"charlie" 37, "beta" 12}
+   "beta" {"alpha" 12, "delta" 35}
+   "charlie" {"alpha" 37, "echo" 51, "delta" 42}
+   "delta" {"beta" 35, "charlie" 42, "foxtrot" 17}
+   "echo" {"charlie" 51, "foxtrot" 33}
+   "foxtrot" {"echo" 33, "delta" 17}})
+
+(declare a*-seq, next-a*-path, unseen?, step-factory, rpath, cmp-step)
+
+(defn a*
+  "A sequence of paths from `src` to `dest`, shortest first, within the supplied `graph`.
+  If the graph is weighted, supply a `distance` function. To make use of A*, supply a
+  heuristic function. Otherwise performs like Dijkstra's algorithm."
+  [graph src dest & {:keys [distance heuristic]}]
+  (let [init-adjacent (sorted-set-by cmp-step {:node src :cost 0 :entered 0})]
+    (a*-seq graph dest init-adjacent
+            (or distance (constantly 1))
+            (or heuristic (constantly 0)))))
+
+(defn a*-seq
+  "Construct a lazy sequence of calls to `next-a*-path`, returning the shortest path first."
+  [graph dest adjacent distance heuristic]
+  (lazy-seq
+    (when-let [[path, adjacent'] (next-a*-path graph dest adjacent distance heuristic)]
+      (cons path (a*-seq graph dest adjacent' distance heuristic)))))
+
+(defn next-a*-path [graph dest adjacent f-cost f-heur]
+  (when-let [{:keys [node] :as current} (first adjacent)]
+    (let [path (rpath current)
+          adjacent' (disj adjacent current)] ;; "pop" the current node
+      (if (= node dest)
+        [(reverse path), adjacent']
+        (let [last-idx (or (:entered (last adjacent')) 0)
+              factory (step-factory current last-idx f-cost f-heur dest)
+              xform (comp (filter (partial unseen? path)) (map-indexed factory))
+              adjacent'' (into adjacent' xform (get graph node))]
+          (recur graph dest adjacent'' f-cost f-heur))))))
+
+(defn unseen? [path node]
+  (not-any? #{node} path))
+
+(defn step-factory [parent last-insertion cost heur dest]
+  (fn [insertion-idx node]
+    {:parent parent
+     :node node
+     :entered (+ last-insertion (inc insertion-idx))
+     :cost (+ (:cost parent) (cost (:node parent) node) (heur node dest))}))
+
+(defn rpath [{:keys [node parent]}]
+  (lazy-seq
+    (cons node (when parent (rpath parent)))))
+
+(defn cmp-step [step-a step-b]
+  (let [cmp (compare (:cost step-a) (:cost step-b))]
+    (if (zero? cmp)
+      (compare (:entered step-a) (:entered step-b))
+      cmp)))
